f328c32118295bd68a429a569df947666d9df06a
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-12-07 01:30:05 +0530

    changes in app.jsx by merging file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,17 @@ import MainPage from "./pages/MainPage";
 import ShareExperienceForm from "./pages/ShareExperienceForm";
 import AboutPage from "./pages/AboutPage";
 import SignupPage from "./pages/SignupPage";
-import { AuthProvider } from "./contexts/AuthContext";
+
 export default function App() {
   return (
     <BrowserRouter>
-     <AuthProvider>
       <Routes>
-        <Route path="/home" element={<MainPage/>} />
+        <Route path="/" element={<MainPage/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/share-experience" element={<ShareExperienceForm />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
-      </AuthProvider>
     </BrowserRouter>
   );
 }

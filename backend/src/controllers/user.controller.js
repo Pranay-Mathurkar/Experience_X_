bf6a3858af552c5c1f62b28d5265a912579dd654
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-12-06 23:37:41 +0530

    changes in controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -97,7 +97,7 @@ const login = async (req, res) => {
       user: {
         id: user._id,
         name: user.name,
-        username: user.username,
+        
         email: user.email,
       },
     });

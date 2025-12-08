b192cdbd989c39df8e3ab339d088718a9dd4aeba
VishnuPrajapati7666 <prajapativishnumunna@gmail.com>
2025-12-09 05:22:35 +0530

    change
    Uncommented the MainPage component and its imports.

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,24 +1,4 @@
-// import React from "react";
 
-
-
-// import { Navbar } from "../components/common/Navbar";
-// import { SearchBar } from "../components/common/SearchBar";
-// import { CompanyGrid } from "../components/mainPage/CompanyGrid";
-// import { AddCompanyButton } from "../components/mainPage/AddCompanyButton";
-// import { Footer } from "../components/common/Footer";
-
-// export default function MainPage() {
-//   return (
-//     <div className="min-h-screen w-full bg-white">
-//       <Navbar/>
-//       <SearchBar />
-//       <CompanyGrid />
-//       <AddCompanyButton />
-//       <Footer/>
-//     </div>
-//   );
-// }
 
 
 import React, { useState } from "react";
@@ -29,7 +9,7 @@ import { AddCompanyButton } from "../components/mainPage/AddCompanyButton";
 import { Footer } from "../components/common/Footer";
 
 export default function MainPage() {
-  const [search, setSearch] = useState(""); // ✅ GLOBAL SEARCH
+  const [search, setSearch] = useState("");
 
   return (
     <div className="min-h-screen w-full bg-white">

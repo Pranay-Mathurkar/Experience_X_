5520358202ac9325f700f8ef65a919d72ab9b61d
Arpit Kumar <5pm2022arpitkumar@gmail.com>
2025-12-08 22:39:18 +0530

    MAde chnages to ui

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Navbar } from "../components/common/Navbar";
 import { SearchBar } from "../components/common/SearchBar";
+
 import { CompanyGrid } from "../components/mainPage/CompanyGrid";
 import { AddCompanyButton } from "../components/mainPage/AddCompanyButton";
+
+
 import { Footer } from "../components/common/Footer";
 
 export default function MainPage() {
